0b3e9eac73f06f3ec807e3c715193b95fc4285bf
Douglas S. Green <douglas.s.green@protonmail.com>
2026-02-22 19:18:23 -0500

    updating config

---
+++ rector.php
@@ -0,0 +1,57 @@
+<?php
+// version: 1.0.0
+// modified: 2026-02-20
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
+use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/bin',
+        __DIR__ . '/card',
+        __DIR__ . '/cards',
+        __DIR__ . '/deck',
+        __DIR__ . '/decks',
+        __DIR__ . '/header.php',
+        __DIR__ . '/help',
+        __DIR__ . '/index.php',
+        __DIR__ . '/languages',
+        __DIR__ . '/src',
+    ])
+    ->withPhpSets(php83: true)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+        phpunitCodeQuality: true,
+    )
+    ->withComposerBased(
+        symfony: true,
+    )
+    ->withSkip([
+        // Empty is OK.
+        DisallowedEmptyRuleFixerRector::class,
+
+        // This rule always injects Session into AppContainer, breaking unit tests.
+        NewInInitializerRector::class => [
+            __DIR__ . '/src/AppContainer.php',
+        ],
+
+        // These tags are still needed.
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class,
+        RemoveUselessVarTagRector::class,
+    ])
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withPhpVersion(PhpVersion::PHP_83);
